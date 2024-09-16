db78727102a2f92f51d70679b00cb13c0028c70a
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-09-16 16:50:37 +0000

    refactor footer into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,18 +11,7 @@ function App() {
       <div className="technologies"></div>
       <div className="services"></div>
 
-      <footer className="footer">
-        {/* Use Swiper.js for carosels */}
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d860.2264990404077!2d-91.06729152684998!3d30.41040885049928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8626a38353854649%3A0x40dc56203128343!2sPro-Tech%20Dental%20Arts!5e0!3m2!1sen!2sus!4v1726441651557!5m2!1sen!2sus"
-          width="400"
-          height="300"
-          style={{ border: 0 }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </footer>
+      <Footer />
     </>
   );
 }
