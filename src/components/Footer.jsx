5e1e4514379c5c0fc59895c88c4116b9e6fc8c95
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-09-16 19:22:57 +0000

    !TODO -> hookup form to send email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import "./Footer.css";
-
+//! TODO - hookup form to send email to protechdental arts on submission 
 const Footer = () => {
   return (
     <section className="footer">
@@ -16,15 +16,15 @@ const Footer = () => {
           </form>
         </div>
         <div className="right">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d860.2264990404077!2d-91.06729152684998!3d30.41040885049928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8626a38353854649%3A0x40dc56203128343!2sPro-Tech%20Dental%20Arts!5e0!3m2!1sen!2sus!4v1726441651557!5m2!1sen!2sus"
-          width="400"
-          height="300"
-          style={{ border: 0 }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d860.2264990404077!2d-91.06729152684998!3d30.41040885049928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8626a38353854649%3A0x40dc56203128343!2sPro-Tech%20Dental%20Arts!5e0!3m2!1sen!2sus!4v1726441651557!5m2!1sen!2sus"
+            width="400"
+            height="300"
+            style={{ border: 0 }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
           <article className="info">
             <p className="faded">address</p>
             <p>4444 Arnold Lane</p>
@@ -41,7 +41,7 @@ const Footer = () => {
             <p>protechdentalarts@yahoo.com</p>
           </article>
         </div>
-    </footer>
+      </footer>
     </section>
   );
 };
