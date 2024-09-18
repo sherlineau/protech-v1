4d51674a31ae139bd86d9bac2fee1895e6402130
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-09-18 16:03:01 +0000

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,24 @@
 import "./Hero.css";
+import Div100vh from "react-div-100vh";
 
 const Hero = () => {
-  return (
-    <div className="hero">
-      <img src="" alt="" />
-      <h2>Pro-Tech Dental Arts</h2>
-    </div>
-  )
-}
 
-export default Hero
+  return (
+    <Div100vh>
+      <section className="hero">
+        <h1 className="hero-header text-accent bg-primary">
+          Pro-Tech Dental Arts
+        </h1>
+
+        <div className="hero-content" >
+          <div>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+          </div>
+          <img src="placeholder.png" alt="" className="hero-img" />
+        </div>
+      </section>
+    </Div100vh>
+  );
+};
+
+export default Hero;
