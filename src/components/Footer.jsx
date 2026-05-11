a5614095b153b47b06ecb77d78d9ac0b0a4e7c49
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2026-05-11 14:30:55 +0000

    feat(footer): add lab slip download button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ import Form from "./Form";
 const Footer = () => {
   return (
     <footer className="footer container">
-
       <div className="right">
         <h2>Pro-Tech Dental Arts</h2>
         <article className="info">
@@ -24,6 +23,8 @@ const Footer = () => {
             protechdentalarts@yahoo.com
           </a>
         </article>
+        <button className="btn bg-primary text-accent"><a href="/Lab_Slip.pdf" download>Download Lab Slip</a></button>
+        </div>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d860.2264990404077!2d-91.06729152684998!3d30.41040885049928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8626a38353854649%3A0x40dc56203128343!2sPro-Tech%20Dental%20Arts!5e0!3m2!1sen!2sus!4v1726441651557!5m2!1sen!2sus"
           width="500"
@@ -31,8 +32,8 @@ const Footer = () => {
           style={{ border: 0 }}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
+          className="left"
         ></iframe>
-      </div>
     </footer>
   );
 };
